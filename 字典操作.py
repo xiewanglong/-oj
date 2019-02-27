2890b6a9383f2742fe063932521f4d406463e3de
xiewanlong <47933781+xiewanglong@users.noreply.github.com>
2019-02-27 14:17:20 +0800

    Create 字典操作.py

---
+++ 字典操作.py
@@ -0,0 +1,12 @@
+#字典操作1使用函数dict实现
+items = [('name','Gumby'),('age',42)]
+d = dict(items)
+print(d)
+print(d['name'])
+#字典操作2
+items = {'name':'age','Gumby':42}
+print(items['name'])
+x = {}
+x[42] = 'Foobar'
+print(x)
+print(x[42])
