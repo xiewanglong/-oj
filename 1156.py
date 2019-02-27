c6130531662b098e82d319d70f88f51fa8b573bc
xiewanlong <47933781+xiewanglong@users.noreply.github.com>
2019-02-27 14:17:08 +0800

    Update 1156.py

--- 1156.py
+++ 1156.py
@@ -1,8 +1,15 @@
 x = input()
 b = ""
+m = 0
+a = ['s','x','o']
+for i in range(3):
+    if x[-1] == a[i]:
+        m = 1
+    if x[-2] + x[-1] == 'ch' or x[-2] + x[-1] == "sh":
+        m = 1
 if x[-1] == 'y':
     b = x[0:-1] + 'ies'
-elif x[-1] == {s,x,ch,sh,o}:
+elif m == 1:
     b = x + 'es'
 else:
     b = x + 's'
